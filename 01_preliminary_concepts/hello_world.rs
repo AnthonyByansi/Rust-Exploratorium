a7b295e47a6761a26cd9622b9c62171bf552645b
Anthony <byansianthony@gmail.com>
2023-10-03 19:46:11 +0300

    add the fundamental Implementation

---
+++ 01_preliminary_concepts/hello_world.rs
@@ -0,0 +1,24 @@
+// hello_world.rs
+fn main(){
+    println!("Hello World!");
+}
+
+// This is a Rust program that prints a message to the console.
+
+// The `fn` keyword is used to define a function in Rust. In this case, we're defining the `main` function, which is the entry point of the program.
+
+fn main() {
+    // This line is the start of the `main` function. Code execution begins here.
+
+    // `println!` is a Rust macro used for printing text to the console. The `!` indicates that it's a macro, not a regular function.
+
+    // Inside the `println!` macro, we have a string literal enclosed in double quotes: "Hello, World!". This is the message we want to print.
+
+    // The `println!` macro takes the message string and prints it to the console, followed by a newline character to start a new line.
+
+    println!("Hello, World!");
+
+    // This line is the end of the `main` function.
+
+    // When the program runs, it will execute the code inside the `main` function. As a result, "Hello, World!" will be printed to the console.
+}
