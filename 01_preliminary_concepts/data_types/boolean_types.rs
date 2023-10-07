96be279915860462343600200d0428689d791465
Byansi Anthony <101401469+AnthonyByansi@users.noreply.github.com>
2023-10-08 00:45:34 +0300

    Create boolean_types.rs

---
+++ 01_preliminary_concepts/data_types/boolean_types.rs
@@ -0,0 +1,10 @@
+fn main(){
+    // Boolean Types
+    
+    let is_rust_cool: bool = true;
+    if is_rust_cool {
+        println!("Rust is cool! 😍😘");
+    }else{
+        println!("Rust isn't cool 😒");
+    }
+}
