1940c9798f3bcc76f4734815b7a361c18e4dbd82
Byansi Anthony <101401469+AnthonyByansi@users.noreply.github.com>
2023-10-08 01:11:33 +0300

    Add boolean implementations

--- 01_preliminary_concepts/data_types/boolean_types.rs
+++ 01_preliminary_concepts/data_types/boolean_types.rs
@@ -7,4 +7,41 @@ fn main(){
     }else{
         println!("Rust isn't cool 😒");
     }
+    
+    // boolean Expressions
+    /*
+    Boolean types are often used in conditional statements(Like `if`, `else`, `while`, `match` etc)
+    to control the flow of te program based on conditions. They can include comparisons (e.g `==`, `!=`, `<`, `>`, `<=`, `>=`) and logical operators (e.g `&&` for AND, `||` for logical OR, `!` for logical NOT)
+    
+    */
+    
+    let x = 5;
+    let y = 10;
+    if x < y && x != 0 {
+        // Rust gets Cool 👌
+    }
+    // Boolean Operators
+    let is_sunny = true;
+    let is_weekend = false;
+    
+    if is_sunny && !is_weekend {
+        // This block gets executed as the conditions are true.
+    }
+    
+    // Boolean Functions and Matches 
+    
+    let my_string = "Rust is Cool";
+    let starts_with_rust = my_string.starts_with("Rust");
+    // starts_with_hello will be true
+    
+    
+    // pattern Matching with Booleans
+    
+    let is_rust_cool = true;
+    match is_rust_cool {
+        true => println!("Rust is cool 💕"),
+        false => println!("Rust isn't cool nigga! 😒"),
+    }
+    
+    
 }
