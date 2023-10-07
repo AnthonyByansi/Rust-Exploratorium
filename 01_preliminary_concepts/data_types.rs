fd324f85bcc6cbf1f903420d4085c083d601c31d
Byansi Anthony <101401469+AnthonyByansi@users.noreply.github.com>
2023-10-08 00:08:50 +0300

    Create data_types.rs

---
+++ 01_preliminary_concepts/data_types.rs
@@ -0,0 +1,70 @@
+func main(){
+    /*
+    Data types: Rust provides a rich set of data types to help you write safe and efficent code.
+    1. Integer types: 
+    `i8`, `i16`, `i32`, `i64`, `i128`: Signed integers with different bit sizes
+    `u8`, `u16`, `u32`, `u64`, `u128`: Unsigned integers with different bit sizes
+    `isize` and `usize`: Platform-dependent signed and unsigned integers
+    
+    2. FLoating-Point Types:
+    `f32`: 32-bit floating point number(single precision)
+    `f64`: 64-bit floating-point number (Double-precision)
+    
+    3. Boolean Type:
+    `bool`: Represents either true or false
+    
+    4.Character Type:
+    `char`: Represents a single Unicode Character
+    
+    5. String Types
+    `&str`: A String Slice(a Reference to a string) that is oftern uses for read-only access
+    `String`: A growable heap-allocated string
+    
+    6. Array Types:
+    `[T, N]`: A fixed-size array of elements of type T
+    
+    7. Slice Types:
+    `&[T]`: A dynamically-sized view into a contiguous sequence of elements of type `T`
+    
+    8. Tuple Types:
+    `(T1, T2, ...)`: An ordered collection of elements, each with its own type.
+    
+    9. Function Types:
+    `fn`: Represents a function type
+    
+    10. Pointer Types:
+    `*const T` and `*mut T`: Raw pointers to data type `T`
+    
+    11. Reference Types:
+    `&T`: Immutable reference to data of type `T`
+    `&mut T`: Immutable reference to data of type `T`
+    
+    12. Option Types:
+    `Option<T>`: Represents an Optional value, either `some(T)` or `None`
+    
+    13. Result Type:
+    `Result<T, E>: Represents a result of an operation, either `Ok(T)` with a value of type `T` or `Err(E)` with an error of type `E`.
+    
+    14. Enumeration Types(Enums):
+    `enum`: A Custom data type that represents one of a finite set of values
+
+    15. Struct Types:
+    `struct`: A custom data type that can group multiple values with different types
+    
+    16. Union Types:
+    `union`: A type that can hold one of its fields at a time, useful for low-level memory manipulation.
+    
+    17. Closures:
+    `fn`, `fnMut`, and `fnOnce`: Function-Like types that used to define closures
+    
+    18. Custom Types:
+     we can define custom Data types using `struct` and `enum` declarations
+     
+    19. Slices and String Slices:
+     These are references ti parts of arrays and strings, respectively
+     
+    20. References to Traits:
+     References to Traits(e.g `&dyn Trait`) enable trait objects for dynamix dispatch
+     
+     */
+}
